ae9c8049af58790860603bfca5c8c51fab54db44
DennsHartstock <dennis.hartstock@web.de>
2022-03-01 06:58:40 +0100

    added QuizFragment

--- app/src/main/java/com/example/soccerquiz/QuizFragment.kt
+++ app/src/main/java/com/example/soccerquiz/QuizFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.example.soccerquiz.databinding.FragmentQuizBinding
 import com.example.soccerquiz.databinding.FragmentWelcomeScreenBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -15,10 +16,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [WelcomeScreenFragment.newInstance] factory method to
+ * Use the [QuizFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class WelcomeScreenFragment : Fragment() {
+class QuizFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,7 +37,7 @@ class WelcomeScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentWelcomeScreenBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome_screen, container, false)
+        val binding = DataBindingUtil.inflate<FragmentQuizBinding>(inflater, R.layout.fragment_quiz, container, false)
         return binding.root
     }
 
@@ -47,12 +48,12 @@ class WelcomeScreenFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment WelcomeScreenFragment.
+         * @return A new instance of fragment QuizFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            WelcomeScreenFragment().apply {
+            QuizFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
