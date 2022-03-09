cc943fa15f6c77150f0cc67b47bef7044c3e1d18
DennsHartstock <dennis.hartstock@web.de>
2022-03-09 07:10:29 +0100

    implemented menu on Toolbar

--- app/src/main/java/com/example/soccerquiz/QuizFragment.kt
+++ app/src/main/java/com/example/soccerquiz/QuizFragment.kt
@@ -1,13 +1,12 @@
 package com.example.soccerquiz
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.soccerquiz.databinding.FragmentQuizBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -140,7 +139,20 @@ class QuizFragment : Fragment() {
         }
 
         (activity as AppCompatActivity).supportActionBar?.title = "Soccer Quiz"
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            requireView().findNavController()
+        ) || super.onOptionsItemSelected(item)
     }
 
     private fun getRandomQuizItem() {
